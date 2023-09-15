88d9c1df04e75e788aa6c388d16620e0e8e3b484
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-15 18:54:16 +0530

    header

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import phone from "../images/intro-phone 1.png";
+
+function Home() {
+  return (
+    <>
+      <div className="flex flex-col text-center relative">
+        <p className="text-cash text-white  font-black">CASH</p>
+
+        <img
+          src={phone}
+          alt="phone"
+          className="left-56 -bottom-24 w-96 absolute h-phone top-24"
+        />
+        <p className="text-cash text-white  font-black absolute -bottom-56 left-24">
+          APP
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../images/logo.svg";
+import { NavLink } from "react-router-dom";
+import eye from "../images/Layer 2.svg";
+
+function Menu() {
+  return (
+    <header className="bg-black px-10 py-5 flex justify-between">
+      <img src={logo} alt="logo" />
+      <div className="text-white flex justify-between gap-5">
+        <NavLink>SIGN IN</NavLink>
+        <NavLink>LEGAL</NavLink>
+        <NavLink>LICENCES</NavLink>
+        <NavLink>SECURITY</NavLink>
+        <NavLink>CAREERS</NavLink>
+        <NavLink>PRESS</NavLink>
+        <NavLink>SUPPORT</NavLink>
+        <NavLink>STATUS</NavLink>
+        <NavLink>CODEBLOG</NavLink>
+      </div>
+      <img src={eye} alt="logo" />
+    </header>
+  );
+}
+
+export default Menu;
