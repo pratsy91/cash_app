9eeb7b072305e1ebd017fb664232ee292c639532
pratsy91 <pratapsinghchouhan412@gmail.com>
2023-09-19 15:55:43 +0530

    footer updated

--- src/components/FooterBox.jsx
+++ src/components/FooterBox.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function FooterBox({ text, image }) {
   return (
     <div
-      className="border border-white rounded-md py-1 px-2 flex justify-center gap-6 w-44 
-    h-10 items-center"
+      className="border border-white rounded-md py-1 px-2 flex justify-center gap-6 w-38 
+    h-14 items-center"
     >
       <img src={image} alt={text} />
       <p className="text-white text-2xl font-bold">{text}</p>
